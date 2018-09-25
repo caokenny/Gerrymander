cfc161cf3d594243821d188fef6e00c1696280e7
Kenny <kennycao96@gmail.com>
2018-09-25 13:35:32 -0400

    All continents are now blacked out except for NA

--- target/classes/static/js/mapjs.js
+++ target/classes/static/js/mapjs.js
@@ -2,12 +2,10 @@ var mymap = L.map('mapid', {zoomControl: false}).setView([37.0902, -95.7129], 4)
 
 var mapboxtile = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/emerald-v8/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    minZoom: 4,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiY2Fva2VubnkiLCJhIjoiY2ptZHhzcmJoMHVlYjNwbW90cm1kZW11bSJ9.C6aOC-2bLmc9SIXXjI0tyQ'
-});
-
-mapboxtile.addTo(mymap);
+}).addTo(mymap);
 
 // mymap.dragging.disable();
 // mymap.touchZoom.disable();
@@ -16,8 +14,16 @@ mapboxtile.addTo(mymap);
 
 //Add geoJSON
 
-L.geoJSON(usageo, {
+var states = L.geoJSON(usageo, {
     style: function () {
-        return {color: "blue"};
+        return {color: "blue"}
+    }
+}).addTo(mymap);
+
+var continents = L.geoJSON(continentsgeo, {
+    style: function (feature) {
+        if (feature.properties.CONTINENT !== "North America") {
+            return {color: "black", opacity: 1, fillColor: "black", fillOpacity: 1};
+        }
     }
 }).addTo(mymap);
