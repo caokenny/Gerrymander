37be0422250d694d5d634372807a98d73704836c
JackieChen <cjackie124@gmail.com>
2018-11-18 17:07:01 -0500

    cleaned neighbor files to properties file.

--- src/main/java/io/redistrict/RegionGrowing/RgUtilities/ColorRandomizer.java
+++ src/main/java/io/redistrict/RegionGrowing/RgUtilities/ColorRandomizer.java
@@ -5,14 +5,14 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class ColorRandomizer {
-    public  Set<Color> pickRandomColors(int numColors)
+    public static Set<Color> pickRandomColors(int numColors)
     {
         Set<Color> result = new HashSet<>();
         IntStream.range(0,numColors).forEach(num -> result.add(generateRandomColor())
         );
         return result;
     }
-    private Color generateRandomColor() {
+    public static Color generateRandomColor() {
         Random random = new Random();
         int r = random.nextInt(256);
         int b = random.nextInt(256);
