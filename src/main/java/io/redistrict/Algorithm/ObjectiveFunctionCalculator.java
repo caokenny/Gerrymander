fd469156749ebbd24f4f92bb1f4857e140997cca
JackieChen <cjackie124@gmail.com>
2018-12-11 15:22:20 -0500

    made objective function calculator (prototype)

--- src/main/java/io/redistrict/Algorithm/ObjectiveFunctionCalculator.java
+++ src/main/java/io/redistrict/Algorithm/ObjectiveFunctionCalculator.java
@@ -3,6 +3,10 @@ package io.redistrict.Algorithm;
 import io.redistrict.Territory.District;
 import io.redistrict.Territory.State;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ObjectiveFunctionCalculator {
     AlgorithmWeights weights;
 
@@ -29,7 +33,37 @@ public class ObjectiveFunctionCalculator {
     }
 
     public double getStateObjectFunction(State state,AlgorithmType type){
-        //TODO
-        return 0.0;
+
+        double compactnessWeight = weights.getCompactness();
+        double popWeight = weights.getPopulationEquality();
+        Map<Integer,District> districtMap;
+        List<Double> popscoreList = new ArrayList();
+        List<Double> compactnessScoreList = new ArrayList<>();
+
+        if(type==AlgorithmType.RG){districtMap = state.getRgdistricts();}
+        else{districtMap=state.getDefaultDistrict();}
+
+        for(District d : districtMap.values()){
+            double area = d.getArea(d.getAllDPrecincts());
+            double perimeter = d.getPerimeter(d.getAllDPrecincts());
+            float ideaPop = state.calculateIdealPop(type);
+            double popScore =(double) d.calculatePopEqualScore(ideaPop);
+            double compactnessScore = d.calculatePolsbyPopper(area,perimeter);
+
+            popscoreList.add(popScore);
+            compactnessScoreList.add(compactnessScore);
+        }
+        double popScoreAvg = getAverageScore(popscoreList);
+        double compactnessScoreAvg = getAverageScore(compactnessScoreList);
+
+        return (compactnessWeight*compactnessScoreAvg)+(popScoreAvg*popWeight);
+    }
+    private double getAverageScore(List<Double> scores)
+    {
+        double total =0;
+        for(double score : scores){
+            total+= score;
+        }
+        return total/((double)scores.size());
     }
 }
